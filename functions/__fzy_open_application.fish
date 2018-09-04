dac7e90a47de41918f9f495ccbfd6db71aa03807
Shinnosuke Sawada <sawada_shinnosuke_xa@cyberagent.co.jp>
2018-09-04 10:03:18 +0900

    debug

--- functions/__fzy_open_application.fish
+++ functions/__fzy_open_application.fish
@@ -1,5 +1,5 @@
 function __fzy_open_application
-    mdfind 'kMDItemKind==アプリケーション' | (__fzycmd) | read -l select
+    mdfind 'kMDItemKind==アプリケーション' | "(__fzycmd)" | read -l select
     if not test -z "$select"
         open $select
     end
--- functions/__fzycmd.fish
+++ functions/__fzycmd.fish
@@ -1,2 +1,5 @@
-set -q FZY_CMD; or set FZY_CMD fzy
-echo $FZY_CMD
+function __fzycmd
+    set -q FZY_CMD; or set FZY_CMD fzy
+    echo $FZY_CMD
+end
+
